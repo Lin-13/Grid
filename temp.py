53b44a59d03a1f1e076894fce77bb45a776c031f
Lin Wenxiang <3065746398@qq.com>
2022-06-04 14:34:05 +0800

    test

--- temp.py
+++ temp.py
@@ -1,9 +1,14 @@
 import NonliearPack as nlp
 import scipy.optimize as opt
-from main import *
+from my_grid import *
 def calcV1(R):
     [x,y,theta]=R
     return nlp.calcV_2d(grid1,x,y,theta)
 print(calcV1([1,1,-0.5]))
+#非线性方法
 a=opt.fmin(calcV1,[0,4,5])
+
 print(a)
+#线性方法
+b=np.dot(np.linalg.inv(grid1.coef),grid1.calc_loadMat())
+print(b)
