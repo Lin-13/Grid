c49d79df33de188659d9e13c8a38aa2699d92635
Lin Wenxiang <3065746398@qq.com>
2022-06-04 13:27:26 +0800

    能量法

--- grid_body_2d_nonlinear.py
+++ grid_body_2d_nonlinear.py
@@ -1,4 +1,14 @@
 import pole2d as pl2
 import grid_body_2d as gb2d
-def calcV(Grid:gb2d.Grid,x,y,theta):
-    
+import numpy as np
+import scipy.optimize as opt
+def calcV(Grid:gb2d.Grid,x,y,theta):#刚体位移广义坐标
+    V=0.0
+    for anchor in Grid.anchors:
+        K=anchor.matrix.mat_K
+        rotate_mat=np.array([[np.cos(theta),-np.sin(theta)],[np.sin(theta),np.cos(theta)]])
+        old=np.array([[anchor.x],[anchor.y]],dtype=np.float64)
+        new=np.dot(rotate_mat,old)+np.array([[x],[y]],dtype=np.float64)
+        delta=new-old
+        V=V+np.dot(delta.T,np.dot(K,delta))
+        return V
