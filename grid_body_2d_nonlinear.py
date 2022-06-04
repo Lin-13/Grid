a69f410ec8424bca5ae06ad5b720d4c1cbdbbbe3
Lin Wenxiang <3065746398@qq.com>
2022-06-04 12:30:15 +0800

    能量法

--- grid_body_2d_nonlinear.py
+++ grid_body_2d_nonlinear.py
@@ -1,6 +1,4 @@
 import pole2d as pl2
 import grid_body_2d as gb2d
-class Grid_nonlear(gb2d.Grid):
-    V=0
-    def __init__(self,grid:gb2d.Grid):
-        self=grid
+def calcV(Grid:gb2d.Grid,alpha,beta,gamma):
+    
