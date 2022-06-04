4d6a479c28175a44613e4fff88bb3b025e32f3b5
Lin Wenxiang <3065746398@qq.com>
2022-06-04 13:50:02 +0800

    V

--- grid_body_2d_nonlinear.py
+++ grid_body_2d_nonlinear.py
@@ -2,7 +2,7 @@ import pole2d as pl2
 import grid_body_2d as gb2d
 import numpy as np
 import scipy.optimize as opt
-def calcV(Grid:gb2d.Grid,x,y,theta):#刚体位移广义坐标
+def calcV(Grid:gb2d.Grid,x,y,theta)->float:#刚体位移广义坐标
     V=0.0
     for anchor in Grid.anchors:
         K=anchor.matrix.mat_K
@@ -11,4 +11,4 @@ def calcV(Grid:gb2d.Grid,x,y,theta):#刚体位移广义坐标
         new=np.dot(rotate_mat,old)+np.array([[x],[y]],dtype=np.float64)
         delta=new-old
         V=V+np.dot(delta.T,np.dot(K,delta))
-        return V
+    return V
