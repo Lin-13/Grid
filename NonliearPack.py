8713e61d355aed8a98650446b001212b73e44746
Lin Wenxiang <3065746398@qq.com>
2022-06-04 14:25:09 +0800

    pack

--- NonliearPack.py
+++ NonliearPack.py
@@ -2,7 +2,7 @@ import pole2d as pl2
 import grid_body_2d as gb2d
 import numpy as np
 import scipy.optimize as opt
-def calcV(Grid:gb2d.Grid,x,y,theta)->float:#刚体位移广义坐标
+def calcV_2d(Grid:gb2d.Grid,x,y,theta)->float:#刚体位移广义坐标
     V=0.0
     for anchor in Grid.anchors:
         K=anchor.matrix.mat_K
@@ -13,5 +13,4 @@ def calcV(Grid:gb2d.Grid,x,y,theta)->float:#刚体位移广义坐标
         grid_loadMat=Grid.calc_loadMat()
         V_load=-np.dot(grid_loadMat.T,np.array([[x],[y],[theta]]))
         V=V+np.dot(delta.T,np.dot(K,delta))+V_load
-        
     return V
--- temp.py
+++ temp.py
@@ -1,9 +1,9 @@
-import grid_body_2d_nonlinear as gb2d_nl
+import NonliearPack as nlp
 import scipy.optimize as opt
 from main import *
 def calcV1(R):
     [x,y,theta]=R
-    return gb2d_nl.calcV(grid1,x,y,theta)
+    return nlp.calcV_2d(grid1,x,y,theta)
 print(calcV1([1,1,-0.5]))
 a=opt.fmin(calcV1,[0,4,5])
 print(a)
