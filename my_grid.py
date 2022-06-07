81108bc7699704d3f3a30aabdb6acb3d4e2c2119
Lin Wenxiang <3065746398@qq.com>
2022-06-07 23:20:22 +0800

    根据之前的更改更新my_grid

--- my_grid.py
+++ my_grid.py
@@ -5,16 +5,16 @@ from scipy import stats as st # 导入统计函数计算概率
 import grid_body_2d as gb2d
 import pole2d as pl2
 grid1=gb2d.Grid()
-pole1=pl2.pole(theta=0)#默认 E=1,A=1,L=1,enable=True
-pole2=pl2.pole(E=1,theta=np.pi/2)
-pole3=pl2.pole(theta=np.pi)
-pole4=pl2.pole(theta=-np.pi/2,E=1)
-grid1.add_anchor(gb2d.anchor(0,0,[pole1,pole2,pole3,pole4]))
-grid1.add_anchor(gb2d.anchor(1,0,[pole1,pole2,pole3,pole4]))
+pole1=pl2.Pole(theta=0)#默认 E=1,A=1,L=1,enable=True
+pole2=pl2.Pole(E=1,theta=np.pi/2)
+pole3=pl2.Pole(theta=np.pi)
+pole4=pl2.Pole(theta=-np.pi/2,E=1)
+grid1.add_anchor(gb2d.Anchor(0,0,[pole1,pole2,pole3,pole4]))
+grid1.add_anchor(gb2d.Anchor(1,0,[pole1,pole2,pole3,pole4]))
 grid1.print_anchors()
 grid1.print_coef()
-F1=gb2d.load(1,0,0,0,loadtype='F')
-M1=gb2d.load(1,loadtype='M')
+F1=gb2d.Load(1,0,0,0,loadtype='F')
+M1=gb2d.Load(1,loadtype='M')
 grid1.add_load(F1)
 #grid1.add_load(M1)
 mat=grid1.calc_loadMat()#计算加载矩阵，[Fx,Fy,M]
