ad7854f6d3d140e5b8e53ec62d5392a5ee667e0d
Lin Wenxiang <3065746398@qq.com>
2022-06-15 15:08:34 +0800

    加入temp.py进度条,优化了矩阵运算

--- temp.py
+++ temp.py
@@ -3,7 +3,7 @@ import scipy.optimize as opt
 import numpy as np
 import matplotlib.pyplot as plt
 from my_grid import *
-
+import tqdm
 def calcV1(R):
     [x,y,theta]=R
     return nlp.calcV_2d(grid1,x,y,theta)
@@ -20,15 +20,19 @@ print(b)#结果
 #参数设置
 grid1.set(10,10,[0.5,0])              #设置质量，矩
 next_fram=np.array([0,0,0,0,0,0])   #初始状态
-it=5000                             #迭代次数
+it=10000                             #迭代次数
 delta_t=0.01                        #时间步长
 frames=np.zeros((it+1,6))
-b=1                                 #衰减因子
+b=0.1                                 #衰减因子
 frames[0]=next_fram
 #开始迭代仿真
+print("开始动力学迭代仿真")
+bar=tqdm.tqdm(total=it)
 for i in range(it):
     next_fram=nlp.next_frame_2d(grid1,next_fram,t=delta_t,b=b,grad_step=0.001)
     frames[i+1]=next_fram
+    bar.update(1)
+bar.close()
 #打印仿真结果
 print("通过动力学仿真\n(衰减因子b={},m={},J={}),100s后位置如下:\n[x,y,theta,vx,vy,omega]:".format(b,grid1.m,grid1.J))
 print(frames[-1])
@@ -36,15 +40,14 @@ print(frames[-1])
 #plot
 title=['x','y','theta','Vx','Vy','omega']
 y_axis=['mm','mm','rad','mm/s','mm/s','rad/s']
+y=np.zeros((it+1,6))
+y=frames.T
 plt.figure(1)
 for i in range(6):
     plt.subplot(2,3,i+1)
     plt.title(title[i])
-    y=np.zeros((it+1))
-    for j in range(it+1):
-        y[j]=frames[j][i]
     t=np.linspace(0,delta_t*it,it+1,endpoint=True)
-    plt.plot(t,y)
+    plt.plot(t,y[i])
     plt.xlabel('time(s)')
     plt.ylabel(y_axis[i])
 plt.show()
--- temp_m.py
+++ temp_m.py
@@ -3,6 +3,7 @@ import scipy.optimize as opt
 import numpy as np
 import matplotlib.pyplot as plt
 from my_grid_3d import *
+import tqdm
 def calcV1(R):
     [x,y,z,appha,beta,gamma]=R
     return nlp.calcV_3d(grid1,x,y,z,appha,beta,gamma)
@@ -17,18 +18,22 @@ b=np.dot(np.linalg.inv(grid1.coef),grid1.calc_loadMat())
 print(b)#结果
 #动态仿真：
 #参数设置
+print("仿真")
 grid1.set(M=10,J=[10,10,10],center=[0,0,0])                #设置质量，矩
 #next_fram=np.array([0,0,0,0,0,0,0,0,0,0,0,0])   #初始状态
 next_fram=np.zeros((12))
 it=10000                             #迭代次数
 delta_t=0.1                         #时间步长
 frames=np.zeros((it+1,12))
-b=5                                 #衰减因子
+b=10                                 #衰减因子
 frames[0]=next_fram
 #开始迭代仿真
+bar=tqdm.tqdm(total=it)#进度条
 for i in range(it):
     next_fram=nlp.next_frame_3d(grid1,next_fram,t=delta_t,b=b,grad_step=0.001)
     frames[i+1]=next_fram
+    bar.update(1)
+bar.close()
 #打印仿真结果
 print("通过动力学仿真\n(衰减因子b={},m={},J={}),100s后位置如下:\n[x,y,theta,vx,vy,omega]:".format(b,grid1.m,grid1.J))
 print(frames[-1])
@@ -36,15 +41,14 @@ print(frames[-1])
 #plot
 title=['x','y','z','alpha','beta','gamma','Vx','Vy','Vz','Ox','Oy','Oz']
 y_axis=['mm','mm','mm','rad','rad','rad','mm/s','mm/s','mm/s','rad/s','rad/s','rad/s']
+y=np.zeros((it+1,12))
+y=frames.T
 plt.figure("迭代次数{},阻尼{}".format(it,b))
 for i in range(12):
     plt.subplot(4,3,i+1)
     plt.title(title[i])
-    y=np.zeros((it+1))
-    for j in range(it+1):
-        y[j]=frames[j][i]
     t=np.linspace(0,delta_t*it,it+1,endpoint=True)
-    plt.plot(t,y)
+    plt.plot(t,y[i])
     plt.xlabel('time(s)')
     plt.ylabel(y_axis[i])
 plt.show()
